83a3acfb36047959c2979d934899fb93c3f087b3
Ujjwaljain16 <jainujjwal1609@gmail.com>
2026-03-23 21:29:00 +0530

    final submission with readme

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,21 @@ public class Main {
     public static void main(String[] args) {
         Pen pen = new Pen(3);
 
+        System.out.println("Step 1: Try write while pen is closed");
         pen.write();
 
+        System.out.println("\nStep 2: Open pen and write until ink gets over");
         pen.start();
         pen.write();
         pen.write();
         pen.write();
         pen.write();
 
+        System.out.println("\nStep 3: Refill and write again");
         pen.refill();
         pen.write();
 
+        System.out.println("\nStep 4: Close pen and try writing");
         pen.close();
         pen.write();
     }
